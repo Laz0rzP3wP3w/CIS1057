b1a2a8ac318263cee27a4b36b7c9d2df92b75e71
Laz0rzP3wP3w <chris.m.tither@gmail.com>
2014-12-12 16:37:00 -0500

    Update doorAlarm.py

--- doorAlarm.py
+++ doorAlarm.py
@@ -6,10 +6,10 @@ GPIO.setmode(GPIO.BOARD)
 channel = 26 #whatever pin we use
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-TOADDR = "2675495302@messaging.sprintpcs.com" # fill in to address (gmail only)
-FROMADDR = "iamyourdeaddad@gmail.com" #fill in from address
+TOADDR = "" # fill in to address (gmail only)
+FROMADDR = "" #fill in from address
 LOGIN = FROMADDR
-PASSWORD = "ROCKBAND" #fill in password
+PASSWORD = "" #fill in password
 msg = "Door has been opened."
 
 def send_email(TOADDR, LOGIN, PASSWORD, FROMADDR, msg):
